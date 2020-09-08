5dae3b52bb9215edb68d7d258626ab0ef5cf32ae
Felix Pojtinger <felix@pojtinger.com>
2020-09-08 18:19:02 +0200

    feat: Add basic implementation

--- main.go
+++ main.go
@@ -2,32 +2,91 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 	"sync"
 )
 
 func main() {
 	// Parse flags
-	outpath := flag.String("outpath", "portstopackets.csv", "File to write the ports to")
+	inFilePath := flag.String("in", "/etc/ports2packets/service-names-port-numbers.csv", "Path to the CSV input file containing the registered services. Download from https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml")
+	outFilePath := flag.String("out", "ports2packets.csv", "Path of the generated CSV output file")
 
 	flag.Parse()
 
-	// TODO: Use https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml as CSV for the base
+	// Read registered ports from CSV file
+	csvFile, err := os.Open(*inFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	registeredPorts := make(map[int]bool)
+
+	csvReader := csv.NewReader(csvFile)
+	currentLine := 0
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Skip header
+		if currentLine != 0 {
+			// We only care about UDP ports, as we can use net.Dial to check if TCP ports are open
+			if record[2] == "udp" {
+				if record[1] != "" {
+					rangeEnd := strings.Split(record[1], "-")
+
+					startPort, err := strconv.Atoi(rangeEnd[0])
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					if !registeredPorts[startPort] {
+						registeredPorts[startPort] = true
+					}
+
+					if len(rangeEnd) > 1 {
+						endPort, err := strconv.Atoi(rangeEnd[1])
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						delta := endPort - startPort
+
+						for i := 1; i <= delta; i++ {
+							if !registeredPorts[startPort+i] {
+								registeredPorts[startPort+i] = true
+							}
+						}
+					}
+				}
+			}
+		}
+
+		currentLine++
+	}
+
+	// Listen on UDP ports and get the packets sent by nmap
 	outFileContent := ""
 	listenHost := "localhost"
 
-	listenStartPort := 0
-	listenEndPort := 1023
-
-	// Listen on UDP ports and get the packets sent by nmap
 	go func() {
 		var wg sync.WaitGroup
-		for port := listenStartPort; port <= listenEndPort; port++ {
+		for port := range registeredPorts {
 			wg.Add(1)
 
 			go func(wg *sync.WaitGroup, innerPort int) {
@@ -46,8 +105,6 @@ func main() {
 				packet := base64.StdEncoding.EncodeToString(buf[:n])
 
 				if packet != "" {
-					log.Println(innerPort)
-
 					outFileContent += fmt.Sprintf("%v,%v\n", innerPort, packet)
 				}
 
@@ -61,7 +118,7 @@ func main() {
 	}()
 
 	// Run nmap on known UDP ports
-	cmd := exec.Command("nmap", "-sU", listenHost)
+	cmd := exec.Command("nmap", "–host-timeout", "2000", "-sU", listenHost)
 
 	output, err := cmd.Output()
 	if err != nil {
@@ -69,5 +126,5 @@ func main() {
 	}
 
 	// Write to CSV file
-	ioutil.WriteFile(*outpath, []byte(outFileContent), 0644)
+	ioutil.WriteFile(*outFilePath, []byte(outFileContent), 0644)
 }
